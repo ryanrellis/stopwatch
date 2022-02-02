4c3fa94f7ac04424116145c189c25d904abc25db
ryanrellis <ryanrellis@hotmail.com>
2022-02-01 23:23:29 -0400

    Added 'Lap' functionality

--- lib/stopwatch.dart
+++ lib/stopwatch.dart
@@ -13,6 +13,7 @@ class _StopWatchState extends State<StopWatch> {
   bool isTicking = false;
   int milliseconds = 0;
   Timer? timer;
+  final laps = <int>[];
 
   void _onTick(Timer time) {
     setState(() {
@@ -25,66 +26,107 @@ class _StopWatchState extends State<StopWatch> {
     return '$seconds seconds';
   }
 
-  @override
-  void dispose() {
-    super.dispose();
+  void _lap() {
+    setState(() {
+      laps.add(milliseconds);
+      milliseconds = 0;
+    });
+  }
+
+  void _startTimer() {
+    timer = Timer.periodic(Duration(milliseconds: 100), _onTick);
+    setState(() {
+      isTicking = true;
+      laps.clear();
+    });
+  }
+
+  void _stopTimer() {
     timer!.cancel();
+    setState(() {
+      isTicking = false;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    void _startTimer() {
-      timer = Timer.periodic(Duration(milliseconds: 100), _onTick);
-      setState(() {
-        isTicking = true;
-      });
-    }
-
-    void _stopTimer() {
-      timer!.cancel();
-      setState(() {
-        isTicking = false;
-      });
-    }
-
     return Scaffold(
       appBar: AppBar(
         title: Text("Stopwatch"),
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text(_secondsText(milliseconds)),
-          SizedBox(height: 20),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton(
-                style: ButtonStyle(
-                  backgroundColor:
-                      MaterialStateProperty.all<Color>(Colors.green),
-                  foregroundColor:
-                      MaterialStateProperty.all<Color>(Colors.white),
-                ),
-                onPressed: isTicking ? null : _startTimer,
-                child: Text("Start"),
-              ),
-              SizedBox(
-                width: 20.0,
-              ),
-              TextButton(
-                style: ButtonStyle(
-                    backgroundColor:
-                        MaterialStateProperty.all<Color>(Colors.red),
-                    foregroundColor:
-                        MaterialStateProperty.all<Color>(Colors.white)),
-                onPressed: isTicking ? _stopTimer : null,
-                child: Text("Stop"),
-              )
-            ],
-          ),
+          Expanded(child: _buildCounter(context)),
+          Expanded(child: _buildLapDisplay()),
         ],
       ),
     );
+  }
+
+  Widget _buildCounter(BuildContext context) {
+    return Container(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('Lap ${laps.length + 1}'),
+          Text(_secondsText(milliseconds)),
+          SizedBox(height: 20),
+          _buildControls(),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildControls() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        ElevatedButton(
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all<Color>(Colors.green),
+            foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+          ),
+          onPressed: isTicking ? null : _startTimer,
+          child: Text("Start"),
+        ),
+        SizedBox(
+          width: 20.0,
+        ),
+        ElevatedButton(
+          onPressed: isTicking ? _lap : null,
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all<Color>(Colors.yellow),
+          ),
+          child: Text("Lap"),
+        ),
+        SizedBox(
+          width: 20.0,
+        ),
+        TextButton(
+          style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all<Color>(Colors.red),
+              foregroundColor: MaterialStateProperty.all<Color>(Colors.white)),
+          onPressed: isTicking ? _stopTimer : null,
+          child: Text("Stop"),
+        )
+      ],
+    );
+  }
+
+  Widget _buildLapDisplay() {
+    return ListView(
+      children: [
+        for (int milliseconds in laps)
+          ListTile(
+            title: Text(_secondsText(milliseconds)),
+          ),
+      ],
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    timer!.cancel();
   }
 }
